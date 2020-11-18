64084a241ddef068bc10b414f6ad62d315e9734d
Raul Cesar <raul.teixeira@gmail.com>
2020-11-18 10:22:41 -0300

    links to shop, home, contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 // import './Home';
 import HomePage from '@pages/home/Home';
 import ShopPage from '@pages/shopPage/shopPage';
+import Header from '@components/header/header';
+
 import { Switch, Route } from 'react-router-dom';
 
 // import { Route } from 'react-router-dom';
@@ -11,6 +13,7 @@ import { Switch, Route } from 'react-router-dom';
 function App () {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage}></Route>
                 <Route path='/shop' component={ShopPage}></Route>
