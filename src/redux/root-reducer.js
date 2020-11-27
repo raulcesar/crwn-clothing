ed9b1b06bb2c05892fc352202a39f40e1b7a7837
Raul Cesar <raul.teixeira@gmail.com>
2020-11-27 20:50:04 -0300

    included redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer.js';
 import cartReducer from './cart/cart.reducer.js';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cartReducer']
+};
+const rootReducer = combineReducers({
     userReducer,
     cartReducer
 });
+
+
+export default persistReducer(persistConfig, rootReducer);
